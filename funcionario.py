8368f70c2712d527f51169f8b31650a2eee8783f
matheussteinbach <matheussteimbach4@gmail.com>
2023-09-11 21:30:37 -0300

    Funcionario corrigido

--- funcionario.py
+++ funcionario.py
@@ -12,6 +12,7 @@ class Funcionario(Pessoa):
         self.dependentes.append(Dependente(nome, cpf))
     
     def rem_dependente(self, cpf):
-        for dependente in self.dependentes:
-            if cpf in self.dependentes:
-                self.dependentes.remove(dependente)
+        cpfs = [dependentes.cpf for dependente in self.dependentes]
+        if cpf in cpfs:
+            indice = cpfs.index(cpf)
+            self.dependentes.remove(self.dependentes[indice])
