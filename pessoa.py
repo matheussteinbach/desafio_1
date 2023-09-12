dd13775a34001da8a129fe8347452bb2cd6ccdc7
matheussteinbach <matheussteimbach4@gmail.com>
2023-09-11 21:44:19 -0300

    Abstração iniciada

--- pessoa.py
+++ pessoa.py
@@ -1,6 +1,12 @@
+from abc import ABC, abstractmethod
 
 
-class Pessoa():
+class Pessoa(ABC):
+    @abstractmethod
     def __init__(self, nome: str, cpf: str):
         self.nome = nome
         self.cpf = cpf
+
+    @abstractmethod
+    def mostra_dados():
+        pass
